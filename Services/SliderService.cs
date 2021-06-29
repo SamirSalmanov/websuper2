8e8a41e4db0d01869910757667051545b80552e6
SamirSalmanov <samir_salmanov_1988@mail.ru>
2021-06-28 23:53:09 -0700

    websuper2

---
+++ Services/SliderService.cs
@@ -0,0 +1,26 @@
+﻿using DataAccess;
+using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public class SliderService
+    {
+        private WebsuperContext _context;
+        public SliderService(WebsuperContext context)
+        {
+            _context = context;
+        }
+
+        public List<Slider> GetSliders()
+        {
+            var sliders = _context.Sliders.ToList();
+
+            return sliders;
+        }
+    }
+}
